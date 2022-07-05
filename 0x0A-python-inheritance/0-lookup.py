4d8772a0cabd5b44bbc07d6e7fe674788cb821e4
Ajiboye Adeleye <ajiboyeadeleye080@gmail.com>
2022-07-05 04:52:05 -0700

    function that return available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This module contains a function that returns the available attributes and method of an object
+"""
+
+
+def lookup(obj):
+    """returns available methods and attributes"""
+    return dir(list)
