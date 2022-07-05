24500dc7607acdfe23715a9ca2dd1c0cefaac5a8
Ajiboye Adeleye <ajiboyeadeleye080@gmail.com>
2022-07-05 06:55:18 -0700

    Class MyList that inherit from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""class MyList inherit from list
+"""
+
+
+class MyList(list):
+    """Inherit from list"""
+    def print_sorted(self):
+    """Prints list but sorted
+    (ascending order)
+    """
+        print(sorted(self))
