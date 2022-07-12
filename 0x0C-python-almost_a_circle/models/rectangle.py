d6acf728e27748bdd32e084e6ea62acbccfce272
Ajiboye Adeleye <ajiboyeadeleye080@gmail.com>
2022-07-12 16:37:22 -0700

    rectangle class that inherits from base.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-""" Module rectangle """
-from Base import Base
+from models.base import Base
+""" Module Base """
 
 
 class Rectangle(Base):
-    """ class rectangle """
+    """ class Base """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """ check input """
         super().__init__(id)
         self.width = width
         self.height = height
@@ -14,18 +15,22 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """ check input """
         return self.__width
 
     @property
     def height(self):
+        """ check input """
         return self._height
 
     @property
     def x(self):
+        """ check input """
         return self.__x
 
     @property
     def y(self):
+        """ check input """
         return self.__y
 
     @width.setter
@@ -34,7 +39,7 @@ class Rectangle(Base):
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value <= 0:
-           raise ValueError("width must be > 0")
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @height.setter
@@ -64,10 +69,40 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         self.__y = value
 
-
     def area(self):
+        """ Returns the Area of Rectangle """
         return self.__width * self.__height
 
     def display(self):
-        for I in range()
+        """ checks input """
+        if self.__y > 0:
+            print("\n" * self.__y)
+        for i in range(self.__height):
+            if self.__x > 0:
+                print(" " * self.__x + "#" * self.__width)
+            else:
+                print("#" * self.__width)
 
+    def __str__(self):
+        """ check input """
+        return "[{}] ({:d}) {:d}/{:d} - {:d}/{:d}".format(type(self).__name__, self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args, **kwargs):
+        """ check input """
+        attrs = [self.id, self.__width, self.__height, self.__x, self.__y]
+        var = ('id', 'width', 'height', 'x', 'y')
+        if kwargs is not None and (args is None or len(args) == 0):
+            for key, value in kwargs.items():
+                if key in var:
+                    attrs[var.index(key)] = value
+            (self.id, self.__width, self.__height, self.__x, self.__y) = attrs
+        else:
+            args_aux = list(attrs[i] for i in range(len(args), 5))
+            args_aux2 = list(args) + args_aux
+            (self.id, self.__width, self.__height,
+             self.__x, self.__y) = args_aux2
+
+    def to_dictionary(self):
+        """ checks input """
+        dictionary = {'id': self.id, 'width': self.__width, 'height': self.__height, 'x': self.__x, 'y': self.__y}
+        return dictionary
